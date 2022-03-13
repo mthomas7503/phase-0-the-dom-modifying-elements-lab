775ab4050582e632297df5ff1fdacb63f2530586
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-03-13 14:56:56 -0400

    commit tree 2705581e32b8e7770c0a9a4596ff3700ff701c1d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+const main = document.querySelector('#main')
+
+main.remove()
+
+const header = document.createElement('h1')
+
+document.body.append(header)
+
+function createID(idName) {
+    const h1= document.querySelector('h1')
+    return h1.id = idName;}
+
+createID('victory')
+
+const newHeader = document.getElementById('victory')
+
+newHeader.innerText = "YOUR-NAME is the champion"
